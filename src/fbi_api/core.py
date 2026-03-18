734e40d621137065d2ef1abf971a69d6add19be2
teddythepooh <chuatedjesusc@gmail.com>
2026-03-18 16:12:31 -0500

    multi-parameter querying for get_crime_statistics()

--- src/fbi_api/core.py
+++ src/fbi_api/core.py
@@ -1,7 +1,9 @@
 import os
 import time
 import requests
+import warnings
 import pandas as pd
+import itertools
 from tqdm import tqdm
 from pathlib import Path
 from requests.adapters import HTTPAdapter
@@ -9,19 +11,27 @@ from urllib3.util.retry import Retry
 from .utils import load_yaml
 
 class FBI:
+    '''
+    Law enforcement agencies are uniquely identified by the originating agency identifier (ORI); get_metadata() 
+    returns all ORIs by state. The state abbreviations that can be passed to get_metadata() can be found in 
+    FBI.get_state_abbrs(). To retrieve metadata for all states, simply pass "all" to get_metadata(). Lastly, 
+    the offenses supported in get_crime_statistics()'s offenses parameter can be found in FBI.get_offenses().
+    '''
     base_url = "https://api.usa.gov/crime/fbi/cde"
     config = load_yaml(Path(__file__).parent.joinpath("config.yml"))
+    
+    status_code_forcelist = [502, 503, 504]
     
     def __init__(self, 
                  api_key: str = None,
                  timeout_limit: int = 15,
-                 max_retries: int = 5,
-                 exponential_delay_factor: int = 2):
+                 exponential_delay_factor: int = 2,
+                 max_retries: int = 5):
         '''
         api_key (optional): If no api_key is passed, it is invoked from environment variable "FBI_API_KEY."
         timeout_limit (optional): Number of seconds to wait for a response before timing out. Defaults at 15.
-        max_retries (optional): Number of times to retry a request. Defaults at 5.
         exponential_delay_factor (optional): Factor by which to increase delay between retries. Defaults at 2.
+        max_retries (optional): Number of times to retry a request. Defaults at 5.
         '''
         self.api_key = api_key
         self.timeout_limit = timeout_limit
@@ -30,13 +40,29 @@ class FBI:
 
         self.session = self._build_session()
         self.rate_limit = None
+
+        self._validate_api_config()
+
+    def _validate_api_config(self) -> None:
+        '''
+        Validates API configuration parameters.
+        '''
+        if not self.timeout_limit > 0 or not self.max_retries > 0:
+            raise ValueError("timeout_limit and max_retries should be greater than 0.")
         
+        if self.exponential_delay_factor == 0:
+            warnings.warn("No delay between retries because exponential_delay_factor is 0.")
+
     def _build_session(self) -> requests.Session:
+        '''
+        Instantiates requests session. The API call is retried up to self.max_retries times if status code is in 
+        FBI.status_code_forcelist. The delay between retries is exponential by a factor of self.exponential_delay_factor.
+        '''
         session = requests.Session()
         retry = Retry(
             total = self.max_retries,
             backoff_factor = self.exponential_delay_factor,
-            status_forcelist = [502, 503, 504]
+            status_forcelist = FBI.status_code_forcelist
         )
         session.mount("https://", HTTPAdapter(max_retries = retry))
         
@@ -100,47 +126,67 @@ class FBI:
         if state_abbr == "all":
             results = []
             
-            for state in FBI.get_state_abbrs():
-                print(f"Extracting metadata for {state}...")
+            for state in tqdm(FBI.get_state_abbrs(), desc = "Extracting metadata for all states"):
                 results.append(self._oris_by_state(state))
+            
+            print("Success!")
                 
             return pd.concat(results, ignore_index = True)
         else:
             return self._oris_by_state(state_abbr)
     
-    def get_crime_statistics(self, ori: str, year: int, offense: str) -> pd.DataFrame:
-        '''
-        ori: Originating agency identifier (ORI). To see all valid ORIs, do get_metadata('all').
-        year: The year.
-        offense: To see all valid offenses, do FBI.get_offenses().
-        
-        Extracts the monthly crime statistics reported by an agency.
-        '''
+    def _get_stats(self, ori: str, year: int, offense: str) -> pd.DataFrame:
         try:
             offense_mapping = FBI.get_offenses()[offense]
             api_call = self.get(
                 f"{FBI.base_url}/summarized/agency/{ori}/{offense_mapping}?from=01-{year}&to=12-{year}"
-                )
+            )
         except KeyError:
-            raise KeyError(f"Valid offenses to pass are {", ".join(list(FBI.get_offenses().keys()))}.")
-    
+            raise KeyError(f"Valid offenses to pass are {', '.join(list(FBI.get_offenses().keys()))}.")
+
         last_refresh_date = api_call["cde_properties"]["last_refresh_date"]["UCR"]
-        
+
         _, first_value = next(iter(api_call["offenses"]["actuals"].items()))
 
         crime_statistics = pd.Series(first_value, name = "count").rename_axis("date").reset_index()
-        
+
         month_year_columns = ["month", "year"]
         crime_statistics[month_year_columns] = crime_statistics["date"].str.split("-", expand = True)
         crime_statistics.drop(columns = "date", inplace = True)
-        
+
         crime_statistics["ori"] = ori
         crime_statistics["offense"] = offense
         crime_statistics["last_refresh_date"] = last_refresh_date
-        
+
         for col in month_year_columns:
             crime_statistics[col] = crime_statistics[col].astype(int)
-            
+
         crime_statistics.sort_values(by = month_year_columns, inplace = True)
-        
+
         return crime_statistics[["ori", "month", "year", "offense", "count", "last_refresh_date"]]
+
+    def get_crime_statistics(self, 
+                             ori: str | list, 
+                             year: int | list, 
+                             offense: str | list) -> pd.DataFrame:
+        '''
+        ori: Originating agency identifier (ORI) or list of ORIs. To see all valid ORIs, do get_metadata("all").
+        year: The year or list of years.
+        offense: Offense or list of offenses. To see all valid offenses, do FBI.get_offenses().
+
+        Extracts monthly crime statistics.
+        '''
+        combinations = list(itertools.product(
+            ori if isinstance(ori, list) else [ori], 
+            year if isinstance(year, list) else [year],
+            offense if isinstance(offense, list) else [offense]
+            )
+        )
+
+        print(f"Executing {len(combinations)} quer{'y' if len(combinations) == 1 else 'ies'}...")
+        results = []
+
+        for o, y, off in tqdm(combinations):
+            results.append(self._get_stats(o, y, off))
+
+        return pd.concat(results, ignore_index = True)
